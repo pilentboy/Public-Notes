0c3b5c7f6c1c7ae2383f9225859458fa998ca49c
Mahdi Ebrahimi <sirxmahdi79@gmail.com>
2025-05-12 02:51:33 +0330

    add many new features

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -20,10 +20,10 @@ export async function action({ request }: ActionFunctionArgs) {
       username: formData.get("username"),
       password: formData.get("password"),
     });
+    console.log(data, error);
     if (error) {
       return { error: error[0].msg || error };
     }
-    console.log(data);
 
     return { token: data.token };
   } catch (error) {
